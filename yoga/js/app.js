840f1a01ac20b4b63f2c05f3142e16b49e87e18a
Lily <shushan.simonyan.03@gmail.com>
2020-05-04 16:52:33 +0400

    Add Timer

--- yoga/js/app.js
+++ yoga/js/app.js
@@ -33,4 +33,51 @@ let tab = document.querySelectorAll('.info-header-tab'),
 	        }
 	    }
 	});
+
+	//timer
+	let deadline = '2020-5-5';
+
+	function getTimeRemaining(endTime) {
+		let t = Date.parse(endTime) - Date.parse(new Date()),
+		seconds = Math.floor((t/1000) % 60),
+		minutes = Math.floor((t/1000/60) % 60),
+		hours = Math.floor((t/(1000*60*60)));
+
+		return {
+			'total' : t,
+			hours,
+			minutes,
+			seconds
+		}
+	}
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function addZero(num) {
+			if(num < 10) {
+				return '0' + num;
+			}
+			return num;
+		}
+
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+			hours.textContent = addZero(t.hours);
+			minutes.textContent = addZero(t.minutes);
+			seconds.textContent = addZero(t.seconds);
+			if(t.total <= 0) {
+				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
+			}
+		}
+	}
+
+	setClock('timer', deadline);
 });
