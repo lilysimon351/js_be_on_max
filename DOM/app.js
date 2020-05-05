748636f345dd3b2b4a6a24c48f8850d75fdf3b9f
Lily <shushan.simonyan.03@gmail.com>
2020-05-05 10:26:48 +0400

    http request

--- DOM/app.js
+++ DOM/app.js
@@ -40,5 +40,30 @@ wrapper.removeChild(heart[0])
 
 document.body.replaceChild(btn[1], circle[1])
 
+//json, http request
+
+let inputRub = document.getElementById('rub');
+let inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+	let request = new XMLHttpRequest();
+	// request.open(method, url, async, login, pass)
+	request.open('GET', 'current.json');
+	request.setRequestHeader('Content-type', 'application/json; charsetutf-8');
+	request.send();
+
+	request.addEventListener('readystatechange', () => {
+		if(request.readyState === 4 && request.status === 200) {
+			let data = JSON.parse(request.response);
+
+			inputUsd.value = +inputRub.value / data.usd;
+		} else {
+			inputUsd.value = "Smth went wrong...";
+		}
+	})
+
+})
+
+
 console.log(btn)
 console.dir(oneheart)
